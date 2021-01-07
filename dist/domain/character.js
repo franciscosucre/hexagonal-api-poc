86f4c6a8ae23008256d05fdc5586eac31e7db1ef
franciscosucre <franciscojsucreg@gmail.com>
2021-01-07 14:36:35 +0100

    fdgfdg

--- dist/domain/character.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Character = void 0;
-var Character = /** @class */ (function () {
-    function Character() {
-    }
-    return Character;
-}());
-exports.Character = Character;
